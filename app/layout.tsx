a3c0dbeb1933a8e5f0eb4caf883861cfdff1ac2c
MarcoA366 <marco_95_5@yahoo.com>
2026-03-23 20:37:22 -0300

    md

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import DemoBadge from '@/components/DemoBadge';
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -39,6 +40,7 @@ export default function RootLayout({
     <html lang="es">
       <body className="font-sans antialiased">
         {children}
+        <DemoBadge/>
         <Analytics />
       </body>
     </html>
